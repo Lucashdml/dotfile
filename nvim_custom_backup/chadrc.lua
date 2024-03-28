899ff27a8a04298965ab68f101611199e88fc22d
Lucashdml <141373376+Lucashdml@users.noreply.github.com>
2024-03-27 23:30:08 -0300

    Update chadrc.lua

--- nvim_custom_backup/chadrc.lua
+++ nvim_custom_backup/chadrc.lua
@@ -13,33 +13,33 @@ M.ui = {
   telescope = { style = "bordered" },
   statusline = { separator_style = "block" },
 
-  -- nvdash (dashboard)
-  nvdash = {
-    load_on_startup = true,
-
-    header = {
-      "           ▄ ▄                   ",
-      "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
-      "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
-      "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
-      "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ",
-      "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
-      "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
-      "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
-      "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
-    },
-
-    buttons = {
-      { " Find File", "Spc f f", "Telescope find_files" },
-      { "󰈚 Recent Files", "Spc f o", "Telescope oldfiles" },
-      { "󰈭 Find Word", "Spc f w", "Telescope live_grep" },
-      { " Bookmarks", "Spc m a", "Telescope marks" },
-      { " Themes", "Spc t h", "Telescope themes" },
-      { " Mappings", "Spc c h", "NvCheatsheet" },
-    },
-  },
-
-  cheatsheet = { theme = "grid" }, -- simple/grid
+  -- -- nvdash (dashboard)
+  -- nvdash = {
+  --   load_on_startup = true,
+  --
+  --   header = {
+  --     "           ▄ ▄                   ",
+  --     "       ▄   ▄▄▄     ▄ ▄▄▄ ▄ ▄     ",
+  --     "       █ ▄ █▄█ ▄▄▄ █ █▄█ █ █     ",
+  --     "    ▄▄ █▄█▄▄▄█ █▄█▄█▄▄█▄▄█ █     ",
+  --     "  ▄ █▄▄█ ▄ ▄▄ ▄█ ▄▄▄▄▄▄▄▄▄▄▄▄▄▄ ",
+  --     "  █▄▄▄▄ ▄▄▄ █ ▄ ▄▄▄ ▄ ▄▄▄ ▄ ▄ █ ▄",
+  --     "▄ █ █▄█ █▄█ █ █ █▄█ █ █▄█ ▄▄▄ █ █",
+  --     "█▄█ ▄ █▄▄█▄▄█ █ ▄▄█ █ ▄ █ █▄█▄█ █",
+  --     "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
+  --   },
+  --
+  --   buttons = {
+  --     { " Find File", "Spc f f", "Telescope find_files" },
+  --     { "󰈚 Recent Files", "Spc f o", "Telescope oldfiles" },
+  --     { "󰈭 Find Word", "Spc f w", "Telescope live_grep" },
+  --     { " Bookmarks", "Spc m a", "Telescope marks" },
+  --     { " Themes", "Spc t h", "Telescope themes" },
+  --     { " Mappings", "Spc c h", "NvCheatsheet" },
+  --   },
+  -- },
+  --
+  -- cheatsheet = { theme = "grid" }, -- simple/grid
 }
 
 M.plugins = "custom.plugins"
