89992ed9e5846c141baf0cfffff41b36b3ae7850
Lucashdml <141373376+Lucashdml@users.noreply.github.com>
2024-04-16 17:22:34 -0300

    Update plugins.lua

--- nvim_custom_backup/plugins.lua
+++ nvim_custom_backup/plugins.lua
@@ -104,6 +104,25 @@ local plugins = {
       },
     },
   },
+  {
+        "kdheepak/lazygit.nvim",
+    	cmd = {
+    		"LazyGit",
+    		"LazyGitConfig",
+    		"LazyGitCurrentFile",
+    		"LazyGitFilter",
+    		"LazyGitFilterCurrentFile",
+    	},
+        -- optional for floating window border decoration
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        -- setting the keybinding for LazyGit with 'keys' is recommended in
+        -- order to load the plugin when the command is run for the first time
+        keys = {
+           { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+        }
+    },
   -- To make a plugin not be loaded
   -- {
   --   "NvChad/nvim-colorizer.lua",
